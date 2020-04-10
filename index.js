93cf1dfdef298142543dfe3a71865ed44c36ae77
asesor02 <62575844+asesor02@users.noreply.github.com>
2020-04-10 18:09:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ socket.on('messagedetection', (senderNickname,messageContent) => {
 
 
 
+var PORT = process.env.PORT || 3000;
 
+server.listen(PORT,()=>{
 
-server.listen(3000,()=>{
-
-console.log('Node app is running on port 3000');
+console.log('Server Socket IO Running on port:'+PORT);
 
 });
